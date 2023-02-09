9f3b6f8eba12034c0d1660106744f60d1fbce019
Gary <geehor@gmail.com>
2023-02-09 17:25:27 -0500

    Continue ShortenedUrl model class

--- practice-for-ch-sql-url-shortener-long-practice/URLShortener/app/models/shortened_url.rb
+++ practice-for-ch-sql-url-shortener-long-practice/URLShortener/app/models/shortened_url.rb
@@ -1,5 +1,11 @@
 class ShortenedUrl < ApplicationRecord
     validates :short_url, presence: true, uniqueness: true
-    
 
+    def self.random_code
+        random = SecureRandom.urlsafe_base64
+
+        until !ShortenedUrl.exists?(short_url: random)
+            random = SecureRandom.urlsafe_base64
+        end
+    end
 end
